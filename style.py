353898b95e92454908650ee643281a8be9de447c
Gerhard van der Linde <G00364778@gmit.ie>
2019-10-08 12:44:40 +0100

    add masking and further refinements to correlation heatmap plot

---
+++ style.py
@@ -0,0 +1,13 @@
+from IPython.core.display import HTML
+HTML("""
+<style>
+.output_png {
+    display: table-cell;
+    text-align: center;
+    margin:auto;
+}
+.prompt {
+    display:none;
+}
+</style>
+""")
